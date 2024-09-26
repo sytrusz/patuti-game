0603ae8c8556f191a15ad6310088e0f5e564731c
Nathan Rener Malagapo <nathanrener.malagapo@cit.edu>
2024-09-26 13:24:20 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import ThisIsGame from './components/ThisIsGame'
+
+function App() {
+  return (
+    <>
+      <ThisIsGame />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ThisIsGame.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function gameComponent(){
+    const backgroundImg = `/moves/background.png`;
+    const areaImg = `/moves/background.png`;
+    const bulletHorImg = `/moves/bullet_h.png`;
+    const bulletVerImg = `/moves/bullet_v.png`;
+    const idleImg = `/moves/idle-1.png`;
+
+
+    
+    return { backgroundImg, areaImg, bulletHorImg, bulletVerImg, idleImg };
+}
+
+function ThisIsGame(){
+
+    return{
+
+    }
+}
+
+export default ThisIsGame;
